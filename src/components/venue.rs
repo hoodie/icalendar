e56bdd54fa1dfa1d76bdb1dcc2894d5c1eb59371
Hendrik Sollich <hendrik@hoodie.de>
2025-11-02 21:49:59 +0100

    WIP

--- src/components/venue.rs
+++ src/components/venue.rs
@@ -1,23 +1,40 @@
 use super::*;
 
 /// VVENUE  [(ical-venue)](https://tools.ietf.org/html/draft-norris-ical-venue-01)
+#[deprecated(
+    since = "RFC9073",
+    note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location instead."
+)]
 #[derive(Debug, Default, PartialEq, Eq, Clone)]
 pub struct Venue {
     pub(super) inner: InnerComponent,
 }
+#[allow(deprecated)]
 impl Venue {
     /// Creates a new Venue.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::new instead."
+    )]
     pub fn new() -> Self {
         Default::default()
     }
 
     /// Creates a new Event with a UID.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::with_uid instead."
+    )]
     pub fn with_uid(uid: &str) -> Self {
         Self::new().uid(uid).done()
     }
 
     /// End of builder pattern.
     /// copies over everything
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::done instead."
+    )]
     pub fn done(&mut self) -> Self {
         Venue {
             inner: self.inner.done(),
@@ -28,16 +45,28 @@ impl Venue {
     ///
     /// This specifies the street address of a location. If the location requires a multiple-line
     /// address, they may be separated by an encoded newline "\n".
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::street_address instead."
+    )]
     pub fn street_address(&mut self, address: &str) -> &mut Self {
         self.add_property("STREET-ADDRESS", address)
     }
 
     /// Removes the value of the STREET-ADDRESS `Property`.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::remove_street_address instead."
+    )]
     pub fn remove_street_address(&mut self) -> &mut Self {
         self.remove_property("STREET-ADDRESS")
     }
 
     /// Gets the value of the STREET-ADDRESS `Property`.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::get_street_address instead."
+    )]
     pub fn get_street_address(&self) -> Option<&str> {
         self.property_value("STREET-ADDRESS")
     }
@@ -48,16 +77,28 @@ impl Venue {
     /// location. This property may be used to give additional information about an address that is
     /// not usually considered part of the street address. If the location requires a multiple-line
     /// address, they may be separated by an encoded newline "\n".
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::extended_address instead."
+    )]
     pub fn extended_address(&mut self, address: &str) -> &mut Self {
         self.add_property("EXTENDED-ADDRESS", address)
     }
 
     /// Removes the EXTENDED-ADDRESS `Property`
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::remove_extended_address instead."
+    )]
     pub fn remove_extended_address(&mut self) -> &mut Self {
         self.remove_property("EXTENDED-ADDRESS")
     }
 
     /// Gets the value of the EXTENDED-ADDRESS `Property`.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::get_extended_address instead."
+    )]
     pub fn get_extended_address(&self) -> Option<&str> {
         self.property_value("EXTENDED-ADDRESS")
     }
@@ -65,16 +106,28 @@ impl Venue {
     /// Set the LOCALITY `Property`
     ///
     /// This specifies the city or locality of a venue.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::locality instead."
+    )]
     pub fn locality(&mut self, locality: &str) -> &mut Self {
         self.add_property("LOCALITY", locality)
     }
 
     /// Removes the LOCALITY `Property`
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::remove_locality instead."
+    )]
     pub fn remove_locality(&mut self) -> &mut Self {
         self.remove_property("LOCALITY")
     }
 
     /// Gets the value of the LOCALITY `Property`.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::get_locality instead."
+    )]
     pub fn get_locality(&self) -> Option<&str> {
         self.property_value("LOCALITY")
     }
@@ -82,16 +135,28 @@ impl Venue {
     /// Set the REGION `Property`
     ///
     /// This specifies the region (state, province, canton, etc.) of a location.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::region instead."
+    )]
     pub fn region(&mut self, region: &str) -> &mut Self {
         self.add_property("REGION", region)
     }
 
     /// Removes the REGION `Property`
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::remove_region instead."
+    )]
     pub fn remove_region(&mut self) -> &mut Self {
         self.remove_property("REGION")
     }
 
     /// Gets the value of the REGION `Property`.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::get_region instead."
+    )]
     pub fn get_region(&self) -> Option<&str> {
         self.property_value("REGION")
     }
@@ -99,16 +164,28 @@ impl Venue {
     /// Set the COUNTRY `Property`
     ///
     /// This specifies the country of a location.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::country instead."
+    )]
     pub fn country(&mut self, country: &str) -> &mut Self {
         self.add_property("COUNTRY", country)
     }
 
     /// Removes the COUNTRY `Property`
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::remove_country instead."
+    )]
     pub fn remove_country(&mut self) -> &mut Self {
         self.remove_property("COUNTRY")
     }
 
     /// Gets the value of the COUNTRY `Property`.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::get_country instead."
+    )]
     pub fn get_country(&self) -> Option<&str> {
         self.property_value("COUNTRY")
     }
@@ -116,16 +193,28 @@ impl Venue {
     /// Set the POSTAL-CODE `Property`
     ///
     /// This specifies the postal code of a location.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::postal_code instead."
+    )]
     pub fn postal_code(&mut self, postal_code: &str) -> &mut Self {
         self.add_property("POSTAL-CODE", postal_code)
     }
 
     /// Removes the POSTAL-CODE `Property`
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::remove_postal_code instead."
+    )]
     pub fn remove_postal_code(&mut self) -> &mut Self {
         self.remove_property("POSTAL-CODE")
     }
 
     /// Gets the value of the POSTAL-CODE `Property`.
+    #[deprecated(
+        since = "RFC9073",
+        note = "VVENUE is deprecated in favor of VLOCATION (RFC9073). Use Location::get_postal_code instead."
+    )]
     pub fn get_postal_code(&self) -> Option<&str> {
         self.property_value("POSTAL-CODE")
     }
